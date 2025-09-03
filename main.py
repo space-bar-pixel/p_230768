33a4b4e6f0c3d6ce107d246823f7175be499233f
space-bar-pixel <nantawat1348@gmail.com>
2025-09-03 13:20:14 +0700

    [UPD]main.py

--- main.py
+++ main.py
@@ -14,43 +14,16 @@ def sortList(_list):
     return _list
 
 def mode_select():
-    while True:
-        try:
-            _mode = _input("Use auto input? y/n or yes/no: ").strip().lower()
-            if _mode not in ["y", "n", "yes", "no"]:
-                raise ValueError("Please enter a valid response (y/n or yes/no)")
-                
-            clear_console()
-            return _mode
-        except ValueError as e:
-            print(e)
+    
 
 def round_select():
-    while True:
-        try:
-            _round = int(_input("Enter the number of values: ").strip())
-            if _round <= 0:
-                raise ValueError
-                
-            clear_console()
-            return _round
-        except ValueError:
-            print("Please enter a positive whole number only.")
+   
 
 def auto_Input(_round):
-    global numbers
-    numbers = [random.randint(1, 100) for _ in range(_round)]
+    
 
 def promp_for_list(_round):
-    for _ in range(_round):
-        while True:
-            try:
-                num_Input = int(_input("Enter a number: ").strip())
-                numbers.append(num_Input)
-                clear_console()
-                break
-            except ValueError:
-                print("Please enter a valid number.")
+    
 
 def main():
     while True:
